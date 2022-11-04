889defafdb5d374dc73f9ffbf9a7b16c098beb72
Marc Bernabeu Rodriguez <marc_b_r@hotmail.com>
2022-11-04 13:25:10 +0100

    Ia y spawn implementados. Cambia de Disparo

--- Produccio - UnityProject/Assets/Enemy.cs
+++ Produccio - UnityProject/Assets/Enemy.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
     //The box's current health point total
-    public int currentHealth = 3;
+    public float currentHealth = 3;
+    public float movementSpeed = 5.0f;
 
-    public void Damage(int damageAmount)
+     public NavMeshAgent nav;
+
+     //public Transform target;
+    private GameObject player;
+    private void Start(){
+         nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+         player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    public void Damage(float damageAmount)
     {
         //subtract damage amount when Damage function is called
         currentHealth -= damageAmount;
@@ -18,5 +29,8 @@ public class Enemy : MonoBehaviour
             //if health has fallen below zero, deactivate it 
             gameObject.SetActive(false);
         }
+    }
+    private void Update(){
+        nav.SetDestination(player.transform.position);
     }
 }
--- Produccio - UnityProject/Assets/Scripts/GunRelated/GunRaycast.cs
+++ Produccio - UnityProject/Assets/Scripts/GunRelated/GunRaycast.cs
@@ -4,23 +4,24 @@ using UnityEngine;
 
 public class GunRaycast : MonoBehaviour
 {
-    public int gunDamage = 1;                                            // Set the number of hitpoints that this gun will take away from shot objects with a health script
+    public float gunDamage = 1.5f;                                            // Set the number of hitpoints that this gun will take away from shot objects with a health script
     public float fireRate = 0.25f;                                        // Number in seconds which controls how often the player can fire
     public float weaponRange = 50f;                                        // Distance in Unity units over which the player can fire
-    public float hitForce = 100f;                                        // Amount of force which will be added to objects with a rigidbody shot by the player
+    public float hitForce = 400f;                                        // Amount of force which will be added to objects with a rigidbody shot by the player
     public Transform gunEnd;                                            // Holds a reference to the gun end object, marking the muzzle location of the gun
 
     private Camera fpsCam;                                                // Holds a reference to the first person camera
     private WaitForSeconds shotDuration = new WaitForSeconds(0.02f);    // WaitForSeconds object used by our ShotEffect coroutine, determines time laser line will remain visible
     //private AudioSource gunAudio;                                        // Reference to the audio source which will play our shooting sound effect
-    private LineRenderer laserLine;                                        // Reference to the LineRenderer component which will display our laserline
+    //private LineRenderer laserLine;                                        // Reference to the LineRenderer component which will display our laserline
     private float nextFire;                                                // Float to store the time the player will be allowed to fire again, after firing
 
 
     void Start()
     {
         // Get and store a reference to our LineRenderer component
-        laserLine = GetComponent<LineRenderer>();
+        //laserLine = GetComponent<LineRenderer>();
+      
 
         // Get and store a reference to our AudioSource component
         //gunAudio = GetComponent<AudioSource>();
@@ -48,13 +49,13 @@ public class GunRaycast : MonoBehaviour
             RaycastHit hit;
 
             // Set the start position for our visual effect for our laser to the position of gunEnd
-            laserLine.SetPosition(0, gunEnd.position);
+            //laserLine.SetPosition(0, gunEnd.position);
 
             // Check if our raycast has hit anything
             if (Physics.Raycast(rayOrigin, fpsCam.transform.forward, out hit, weaponRange))
             {
                 // Set the end position for our laser line 
-                laserLine.SetPosition(1, hit.point);
+                //laserLine.SetPosition(1, hit.point);
 
                 // Get a reference to a health script attached to the collider we hit
                 Enemy health = hit.collider.GetComponent<Enemy>();
@@ -70,13 +71,13 @@ public class GunRaycast : MonoBehaviour
                 if (hit.rigidbody != null)
                 {
                     // Add force to the rigidbody we hit, in the direction from which it was hit
-                    hit.rigidbody.AddForce(-hit.normal * hitForce);
+                    hit.rigidbody.AddForce(-hit.normal * hitForce);                    
                 }
             }
             else
             {
                 // If we did not hit anything, set the end of the line to a position directly in front of the camera at the distance of weaponRange
-                laserLine.SetPosition(1, rayOrigin + (fpsCam.transform.forward * weaponRange));
+                //laserLine.SetPosition(1, rayOrigin + (fpsCam.transform.forward * weaponRange));
             }
         }
     }
@@ -88,12 +89,12 @@ public class GunRaycast : MonoBehaviour
         //gunAudio.Play();
 
         // Turn on our line renderer
-        laserLine.enabled = true;
+        //laserLine.enabled = true;
 
         //Wait for .07 seconds
         yield return shotDuration;
 
         // Deactivate our line renderer after waiting
-        laserLine.enabled = false;
+        //laserLine.enabled = false;
     }
 }
